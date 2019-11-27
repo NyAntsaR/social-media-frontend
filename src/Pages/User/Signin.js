b9fcfebbe487b688cc809cfa4bc326843575458c
Ny Antsa Rajaonarison <nyantsa@nys-macbook-pro.lan>
2019-11-26 16:05:24 -0800

    Signin the existing user and redirect to home page if successfully authenticated

--- src/Pages/User/Signin.js
+++ src/Pages/User/Signin.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import { Redirect } from "react-router-dom";
 
-class Signup extends Component {
+class Signin extends Component {
 
     constructor() {
         super();
         this.state = {
-            name: "",
             email: "",
             password: "",
             error: "",
-            open: false
+            redirectToReferer: false
         }
     }
 
@@ -20,32 +20,35 @@ class Signup extends Component {
 
     clickSubmit = event => {
         event.preventDefault();
-        const { name, email, password } = this.state;
+        const { email, password } = this.state;
         const user = {
-            name,
             email, 
             password
         };
 
-        this.signup(user)
+        this.signin(user)
         .then(data => {
             if (data.error) {
                 this.setState({ error : data.error })
             } else {
-                this.setState({
-                    error: "",
-                    name: "",
-                    email: "",
-                    password: "",
-                    open: true
+                // Authenticate user
+                this.authenticate(data, () => {
+                    this.setState({ redirectToReferer: true })
                 })
             }
         });
     }
 
-    signup = (user) => {
+    authenticate = (jwt, next) => {
+        // check if window is not undefined
+        if (typeof window !== "") {
+            localStorage.setItem("jwt", JSON.stringify(jwt));
+        } next();
+    }
+
+    signin = (user) => {
         // Make a post request to the backend
-        return fetch("http://localhost:8080/signup", {
+        return fetch("http://localhost:8080/signin", {
             method: "POST",
             headers: {
                 Accept: "application/json",
@@ -59,19 +62,9 @@ class Signup extends Component {
         .catch(err => console.log(err));
     }
 
-    signupForm = ( name, email, password ) => {
+    signinForm = ( email, password ) => {
         return (
             <form>
-
-                <div className="form-group">
-                    <label className="text-muted"> Name </label>
-                    <input 
-                        onChange = { this.handleChange("name") } 
-                        type="text" 
-                        className="form-control" 
-                        value = { name }
-                    />
-                </div>
 
                 <div className="form-group">
                     <label className="text-muted"> Email </label>
@@ -105,24 +98,24 @@ class Signup extends Component {
 
     render () {
 
-        const { name, email, password, error, open } = this.state;
+        const { email, password, error, redirectToReferer } = this.state;
+
+        if ( redirectToReferer ) {
+            return <Redirect to="/" />
+        }
 
         return (
             <div className="container">
-                <h2 className="mt-5 mb-5">Signup</h2>
+                <h2 className="mt-5 mb-5">SignIn</h2>
 
                 <div className="alert alert-primary" style={{ display: error ? "" : "none" }}>
                     { error }
                 </div>
 
-                <div className="alert alert-info" style={{ display: open ? "" : "none" }}>
-                    New account is successfully created. Please Log In!
-                </div>
-
-                { this.signupForm( name, email, password ) }
+                { this.signinForm( email, password ) }
             </div>
         )
     }
 }
 
-export default Signup;
+export default Signin;
--- src/Router/MainRouter.js
+++ src/Router/MainRouter.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../Pages/Home';
 import Signup from '../Pages/User/Signup';
+import Signin from '../Pages/User/Signin';
 
 const MainRouter = () => (
     <div>
         <Switch>
             <Route exact path="/" component={ Home }></Route>
             <Route exact path="/signup" component= { Signup }></Route>
+            <Route exact path="/signin" component= { Signin }></Route>
         </Switch>
     </div>
 );
