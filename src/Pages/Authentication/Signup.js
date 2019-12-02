ce1ea1b9a5192db27dd443bdea0731cf8ed5b80c
andy ratsirarson <andy.ratsirarson@andys-MacBook-Air.local>
2019-12-02 14:45:34 -0800

    Add sign In link to the successfull created user

--- src/Pages/Authentication/Signup.js
+++ src/Pages/Authentication/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class Signup extends Component {
 
@@ -116,7 +117,7 @@ class Signup extends Component {
                 </div>
 
                 <div className="alert alert-info" style={{ display: open ? "" : "none" }}>
-                    New account is successfully created. Please Log In!
+                    New account is successfully created. Please <Link to="/signin">Sign In</Link>!
                 </div>
 
                 { this.signupForm( name, email, password ) }
