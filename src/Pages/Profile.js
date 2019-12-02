2b7e6d5597a04ab53180552dd0d0747678cac5b5
andy ratsirarson <andy.ratsirarson@andys-MacBook-Air.local>
2019-12-02 12:16:29 -0800

    Fetch user data

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { isAuthenticated } from '../Components/Menu/Menu'
+import { Redirect } from "react-router-dom"
 
 class Profile extends Component {
     constructor() {
@@ -12,10 +13,33 @@ class Profile extends Component {
 
     // Get the user iformation based on the user Id from the backend
     componentDidMount () {
-        console.log("user Id from route params: ", this.props.match.params.userId)
+        const userId = this.props.match.params.userId;
+        fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${ isAuthenticated().token }`
+            }
+        })
+        // handle the response to add to the state
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            if(data.error) {
+                this.setState({ redirectToSignin: true })
+            } else {
+                this.setState({ user: data })
+            }
+        })
+
     }
 
     render() {
+        const redirectToSignin = this.state.redirectToSignin;
+        if(redirectToSignin) return <Redirect to="/signin"/>
+
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">
@@ -23,6 +47,7 @@ class Profile extends Component {
                 </h2>
                 <p>Hello {isAuthenticated().user.name} </p>
                 <p> {isAuthenticated().user.email} </p>
+                <p> {`Joined ${new Date(this.state.user.created).toDateString()}`} </p>
             </div>
         )
     }
