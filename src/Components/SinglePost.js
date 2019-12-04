f89935e8c3cb8a54c0bf7a2ad7f0f6d95a5aa0dc
andy ratsirarson <andy.ratsirarson@andys-air.lan>
2019-12-03 17:14:38 -0800

    Implement like/unlike and add thumb button

--- src/Components/SinglePost.js
+++ src/Components/SinglePost.js
@@ -1,27 +1,61 @@
 import React, { Component } from "react";
-import { singlePost, remove } from '../api/apiPost';
+import { singlePost, remove, like, unlike } from '../api/apiPost';
 import DefaultPost from '../image/default.jpg';
 import { Link, Redirect } from 'react-router-dom';
 import { isAuthenticated } from '../Pages/Authentication/Signout'
-
 
 class SinglePost extends Component {
 
     state = {
         post: '',
-        redirectToHome: false
+        redirectToHome: false,
+        redirectToSignin: false,
+        like: false,
+        likes: 0
     }
+
+    checkLike = likes => {
+        const userId = isAuthenticated() && isAuthenticated().user._id;
+        let match = likes.indexOf(userId) !== -1;
+        return match;
+    };
 
     componentDidMount = () => {
         const postId = this.props.match.params.postId;
         singlePost(postId).then(data => {
-            if(data.error) {
+            if (data.error) {
                 console.log(data.error);
             } else {
-                this.setState({ post: data })
+                this.setState({
+                    post: data,
+                    likes: data.likes.length,
+                    like: this.checkLike(data.likes)
+                });
             }
         });
     }
+
+    likeToggle = () => {
+        if (!isAuthenticated()) {
+            this.setState({ redirectToSignin: true });
+            return false;
+        }
+        let callApi = this.state.like ? unlike : like;
+        const userId = isAuthenticated().user._id;
+        const postId = this.state.post._id;
+        const token = isAuthenticated().token;
+
+        callApi(userId, token, postId).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                this.setState({
+                    like: !this.state.like,
+                    likes: data.likes.length
+                });
+            }
+        });
+    };
 
     deletePost = () => {
         const postId = this.props.match.params.postId;
@@ -45,6 +79,9 @@ class SinglePost extends Component {
     renderPost = (post) => {
         const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
         const posterName = post.postedBy ? post.postedBy.name : "Unknown";
+
+        const { like, likes } = this.state;
+
         return (
             <div className="card col-md-4">
                 <img 
@@ -55,39 +92,54 @@ class SinglePost extends Component {
                     style={{ height: '300px', width: '100%', objectFit: 'cover' }}
                 />
 
+                {like ? (
+                    <h3 onClick={this.likeToggle}>
+                        <i
+                            className="fa fa-thumbs-up text-success bg-dark"
+                            style={{ padding: "10px", borderRadius: "50%" }}
+                        />{" "}
+                        {likes} Like
+                    </h3>
+                ) : (
+                    <h3 onClick={this.likeToggle}>
+                        <i
+                            className="fa fa-thumbs-up text-warning bg-dark"
+                            style={{ padding: "10px", borderRadius: "50%" }}
+                        />{" "}
+                        {likes} Like
+                    </h3>
+                )}
 
-                <div className="card-body">
-                    <p className="card-text">{post.body}</p>
+                <p className="card-text">{post.body}</p>
                     
-                    <br />
+                <br />
 
-                    <p className="font-italic mark">
-                        Posted by <Link to={`${posterId}`}>{posterName}{" "}</Link>
-                        on {new Date(post.created).toDateString()}
-                    </p>
+                <p className="font-italic mark">
+                    Posted by <Link to={`${posterId}`}>{posterName}{" "}</Link>
+                    on {new Date(post.created).toDateString()}
+                </p>
 
-                    <div className="d-inline-block">
-                        <Link 
-                            to={`/`}
-                            className="btn btn-raised btn-primary btn-sm mr-5">
-                                Back to posts
-                        </Link>
+                <div className="d-inline-block">
+                    <Link 
+                        to={`/`}
+                        className="btn btn-raised btn-primary btn-sm mr-5">
+                            Back to posts
+                    </Link>
 
-                        {isAuthenticated().user &&
-                            isAuthenticated().user._id === post.postedBy._id && 
-                            <>
-                                <Link 
-                                    to={`/post/edit/${post._id}`}
-                                    className="btn btn-raised btn-warning btn-sm mr-5">
-                                        Update post
-                                </Link>
+                    {isAuthenticated().user &&
+                        isAuthenticated().user._id === post.postedBy._id && 
+                        <>
+                            <Link 
+                                to={`/post/edit/${post._id}`}
+                                className="btn btn-raised btn-warning btn-sm mr-5">
+                                    Update post
+                            </Link>
 
-                                <button onClick={this.deleteConfirmed} className="btn btn-raised btn-danger">
-                                    Delete  Post
-                                </button>
-                            </>
-                        }   
-                    </div>
+                            <button onClick={this.deleteConfirmed} className="btn btn-raised btn-danger">
+                                Delete  Post
+                            </button>
+                        </>
+                    }   
                 </div>
             </div>
         )
@@ -95,14 +147,18 @@ class SinglePost extends Component {
 
     render () {
 
-        if (this.state.redirectToHome) {
+        const { post, redirectToHome, redirectToSignin } = this.state;
+
+        if (redirectToHome) {
             return <Redirect to={`/`} />;
+        } else if (redirectToSignin) {
+            return <Redirect to={`/signin`} />;
         }
 
-        const {post} = this.state
         return (
             <div className="container">
                 <h2 className="display-2 mt-5 mb-5">{post.title}</h2>
+                
                 { !post ? (
                     <div className="jumbotron text-center">
                         <h2>Loading...</h2>
@@ -110,6 +166,7 @@ class SinglePost extends Component {
                         this.renderPost(post)
                     )
                 }
+
             </div>
         );
     }
